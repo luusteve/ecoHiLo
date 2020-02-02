8cede045afeb7cd3ecb8289f75befffc392dd3b8
Nelson Zeng <nelson.zeng25@gmail.com>
2020-02-01 22:01:03 -0500

    Started menu page

--- Frontend/routes/index.js
+++ Frontend/routes/index.js
@@ -5,4 +5,8 @@ router.get('/', (req, res) => {
     res.render('index')
 })
 
+router.get('/menu', (req, res) => {
+    res.render('menu')
+})
+
 module.exports = router
--- Frontend/server.js
+++ Frontend/server.js
@@ -2,15 +2,14 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
-const indexRouter = require('./routes/index')
-
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static( __dirname + '/public'))
 
-app.use('/', indexRouter)
+app.use('/', require('./routes/index'))
+app.use('/menu', require('./routes/index'))
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server started on port 3000!');
