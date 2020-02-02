c5969cb9b039fcea82698fa3a1d20fc8b1aaed83
Nelson Zeng <nelson.zeng25@gmail.com>
2020-02-02 05:33:21 -0500

    Fixed routing

--- Frontend/routes/index.js
+++ Frontend/routes/index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.render('index')
+    res.render('menu')
 })
 
-router.get('/menu', (req, res) => {
-    res.render('menu')
+router.get('/game', (req, res) => {
+    res.render('game')
 })
 
 router.get('/lose', (req, res) => {
