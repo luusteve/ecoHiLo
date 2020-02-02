820ef78428e99e4fe19a1e840b5878554ff27b77
Nelson Zeng <nelson.zeng25@gmail.com>
2020-02-01 19:30:02 -0500

    Added tritin half screen and styles.css

--- Frontend/server.js
+++ Frontend/server.js
@@ -8,8 +8,10 @@ app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
-app.use(express.static('public'))
+app.use(express.static( __dirname + '/public'))
 
 app.use('/', indexRouter)
 
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server started on port 3000!');
+})
